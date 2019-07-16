c8d07232641369fcdee38576b46ad2dca5d42d3a
joshuaw001 <47309531+joshuaw001@users.noreply.github.com>
2019-07-16 10:27:51 -0500

    Update mkup.js

--- mkup.js
+++ mkup.js
@@ -1,3 +1,9 @@
-// parent function for mkupJS
+// parent function for mkup
 
-function customTag()
+function customTag(tag,action){
+document.createElement(tag)
+Inst = getElementsByTagName(tag)
+for(i=0; i<Inst.length;i++){
+action(Inst[i])
+}
+};
