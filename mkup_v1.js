31e64cb25fdca93cee9c9d2e8fa1a7ba4bae6a97
joshuaw001 <47309531+joshuaw001@users.noreply.github.com>
2021-10-01 08:36:19 -0500

    added customTag() function definition

--- mkup_v1.js
+++ mkup_v1.js
@@ -1,1 +1,9 @@
 //Mkup.js
+
+function customTag(tag,action){
+    document.createElement(tag)
+    Inst = getElementsByTagName(tag)
+    for(i=0; i<Inst.length;i++){
+        action(Inst[i])
+    }
+}
